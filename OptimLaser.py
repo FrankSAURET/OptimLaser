c03d517c8bb23bed9e39a9870e24d6933b16565b
frank4466 <frank.sauret@gmail.com>
2024-10-27 15:37:50 +0100

    versionnage

--- OptimLaser.py
+++ OptimLaser.py
@@ -34,8 +34,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #  0.1 Janvier 2023
 #  0.2 juin 2024
 #  0.2.2 octobre 2024
+# 2024.1 novembre 2024 juste le versionnage
 
-__version__ = "0.2.2"
+__version__ = "2024.1"
 
 import os
 import subprocess
